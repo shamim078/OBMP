1cba01d9c07f5d4a8fe23f80462cd70ff35d71f4
shamim078 <shamim078@gmail.com>
2013-12-28 17:59:33 +1100

    update of kendoui grid for product list

--- OBMP/Controllers/CustomerController.cs
+++ OBMP/Controllers/CustomerController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OBMP.Models;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
 
 namespace OBMP.Controllers
 {
     //[Authorize(Roles = "Customer")]
     public class CustomerController : Controller
     {
+
+        readonly Data.northwindEntities _context = new Data.northwindEntities();
+
         //
         // GET: /Customer/
 
@@ -18,12 +24,34 @@ namespace OBMP.Controllers
         }
 
 
-        //
-        // GET: /Customer/Orders
-
-        public ActionResult Order()
+        // parse the incoming request from Kendo UI into a DataSourceRequest
+        public JsonResult GetProducts([DataSourceRequest]DataSourceRequest request)
         {
-            return View();
+
+            // LINQ query to select products and map to model objects
+            var products = _context.Products.Select(p => new Models.Product
+            {
+                Id = p.ProductID,
+                Name = p.ProductName,
+                UnitsInStock = p.UnitsInStock,
+                UnitPrice = p.UnitPrice,
+                Discontinued = p.Discontinued,
+                Supplier = new Models.Supplier
+                {
+                    Id = p.Supplier.SupplierID,
+                    Name = p.Supplier.CompanyName
+                },
+                Category = new Models.Category
+                {
+                    Id = p.Category.CategoryID,
+                    Name = p.Category.CategoryName
+                }
+            });
+
+            // convert to a DataSourceResponse and send back as JSON
+            //return this.Json(products.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+            return this.Json(products.ToDataSourceResult(request));
+
         }
     }
 }
---
+++ OBMP/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OBMP.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/All
+
+        public ActionResult All()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Order/Accepted
+
+        public ActionResult Accepted()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Order/Confirmed
+
+        public ActionResult Confirmed()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Order/Rejected
+
+        public ActionResult Rejected()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Order/Pending
+
+        public ActionResult Pending()
+        {
+            return View();
+        }
+
+    }
+}
