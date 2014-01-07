9a0faf79f8259f93178beb9dc2c44c71ef19ac33
shamim078 <shamim078@gmail.com>
2014-01-07 14:40:58 +1100

    solution before publish

--- OBMP/Controllers/HomeController.cs
+++ OBMP/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OBMPDataModel;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
 
 namespace OBMP.Controllers
 {
     public class HomeController : Controller
     {
+
+        private FluentModel dbContext;
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            this.dbContext = ContextFactory.GetContextPerRequest();
+            base.Initialize(requestContext);
+        }
+
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -28,5 +41,26 @@ namespace OBMP.Controllers
 
             return View();
         }
+
+        public ActionResult AllPartners([DataSourceRequest]DataSourceRequest request)
+        {
+            var partners = dbContext.Partners.Select(p => new Models.PartnerList
+            {
+                ID = p.ID,
+                Name = p.Name,
+                MSAReference = p.MSAReference,
+                Address = p.Address,
+                PartnerType = p.PartnerType,
+                PartnerShare = (int)p.PartnerShare,
+                PrimaryContact = p.PrimaryContact,
+                DateRegistered = (DateTime)p.DateRegistered
+            });
+
+
+            DataSourceResult result = partners.ToDataSourceResult(request);
+            return Json(result);
+
+        }
+
     }
 }
